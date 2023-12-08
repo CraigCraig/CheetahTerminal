caf70fdc061b1e541bab107dde13007a7bdf0a15
Craig Craig <craig@craig.se>
2023-12-08 00:06:04 -0600

    Fixed ls command

--- Modules/Core/Commands/Ls.cs
+++ Modules/Core/Commands/Ls.cs
@@ -1,18 +1,16 @@
 ﻿namespace CheetahTerminal.Modules.Core.Commands;
 
 using System.IO;
-using System.Text;
 using CheetahTerminal.Commands;
 
 public class Ls() : Command("ls", "")
 {
 	public override CommandResult Execute(CommandContext context)
 	{
-		StringBuilder output = new();
 		foreach (var entry in Directory.GetFileSystemEntries(Terminal.Environment.CurrentDirectory))
 		{
-			_ = output.AppendLine(entry);
+			Terminal.Output.Add(entry);
 		}
-		return new CommandResult(true, output.ToString());
+		return new CommandResult(true, "");
 	}
 }
